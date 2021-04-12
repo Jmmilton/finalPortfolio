0c1e149cac610e2169070e044fb2f1f214b3007a
Jmmilton <jason_milton@hotmail.com>
2021-04-12 15:42:14 -0400

    changed mobile scroll

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const animateTop = document.querySelector('.top')
 const animateBottom = document.querySelector('.bottom')
 const animateArrow = document.querySelector('.arrow')
-const animateAbout = document.querySelector('.about-p')
+// const animateAbout = document.querySelector('.about-p')
 // const animateRoadmap = document.querySelector('.roadmap')
 
 window.onload = function(){
@@ -9,29 +9,45 @@ window.onload = function(){
   animateTop.classList.add('animated', 'fadeInDown')
   animateBottom.classList.add('animated', 'fadeInUp')
   animateArrow.classList.add('animated', 'fadeIn')
-  animateAbout.classList.add('animated', 'fadeInUp')
+  // animateAbout.classList.add('animated', 'fadeInUp')
 //   animateRoadmap.classList.add('animated', 'fadeInUp')
 
 }
 
-var secs = document.querySelectorAll('section');
-var currentSection = 0;
-document.querySelector('#arrow').addEventListener('click', move);
+// var secs = document.querySelectorAll('section');
+// var currentSection = 0;
+// document.querySelector('#arrow').addEventListener('click', move);
 
-function move(e) {
-  if (e.target.classList.contains('next') && currentSection < secs.length) {
-    window.scroll({
-      top: secs[++currentSection].offsetTop,
-      left: 0,
-      behavior: 'smooth'
-    });
-    //    secs[++currentSection].scrollIntoView({ behavior: 'smooth' });
-  } else if (currentSection > 0) {
-    window.scroll({
-      top: secs[--currentSection].offsetTop,
-      left: 0,
-      behavior: 'smooth'
-    });
+// function move(e) {
+//   if (e.target.classList.contains('next') && currentSection < secs.length) {
+//     window.scroll({
+//       top: secs[++currentSection].offsetTop,
+//       left: 0,
+//       behavior: 'smooth'
+//     });
+//     //    secs[++currentSection].scrollIntoView({ behavior: 'smooth' });
+//   } else if (currentSection > 0) {
+//     window.scroll({
+//       top: secs[--currentSection].offsetTop,
+//       left: 0,
+//       behavior: 'smooth'
+//     });
 
-  }
-}
+//   }
+// }
+
+// const div1 = document.getElementsByClassName('one')
+
+// const div2 = div1.nextElementSibling;
+
+// console.log(div1)
+
+
+// animateArrow.addEventListener('click', function(){
+//   console.log
+// })
+
+// function nextPage() {
+//   if()
+// }
+
